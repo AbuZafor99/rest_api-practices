6a5b4f113edb600d1dbc380b83d2c09eb3e38fd4
zaforsadiq987@gmail.com <138440583+AbuZafor99@users.noreply.github.com>
2025-06-22 17:18:39 +0600

    add product from api commit

--- lib/home2.dart
+++ lib/home2.dart
@@ -11,90 +11,87 @@ class Module13 extends StatefulWidget {
 }
 
 class _Module13State extends State<Module13> {
-  final ProductController productController=ProductController();
-
+  final ProductController productController = ProductController();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    setState(() {
-      productController.fetchProducts();
-    });
+    fetchAndSetProducts();
+  }
+
+  void fetchAndSetProducts() async {
+    await productController.fetchProducts();
+    setState(() {}); // This will rebuild the UI with the updated products list
   }
 
   @override
   Widget build(BuildContext context) {
-
-    void productDialog(){
+    void productDialog() {
       TextEditingController productNameController = TextEditingController();
       TextEditingController productQTYController = TextEditingController();
       TextEditingController productImageController = TextEditingController();
-      TextEditingController productUnitPriceController = TextEditingController();
-      TextEditingController productTotalPriceController = TextEditingController();
+      TextEditingController productUnitPriceController =
+          TextEditingController();
+      TextEditingController productTotalPriceController =
+          TextEditingController();
 
-      showDialog(context: context, builder: (context)=>
-        AlertDialog(
-          title: Text("Add Product"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "product name"
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "product image"
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "product quantity"
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "product unit price"
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "product total price"
-                ),
-              ),
-
-              SizedBox(height: 20,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      showDialog(
+        context: context,
+        builder:
+            (context) => AlertDialog(
+              title: Text("Add Product"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  ElevatedButton(
-                      onPressed: (){
-                        Navigator.pop(context);
-                      },
-                      child: Text("Cancel"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent.shade400,
-                      foregroundColor: Colors.white
+                  TextField(
+                    decoration: InputDecoration(labelText: "product name"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "product image"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "product quantity"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "product unit price",
                     ),
                   ),
-                  SizedBox(width: 20,),
-                  ElevatedButton(
-                      onPressed: (){},
-                      child: Text("Add Product"),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent.shade200,
-                        foregroundColor: Colors.white
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "product total price",
                     ),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
+                  ),
 
+                  SizedBox(height: 20),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("Cancel"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.redAccent.shade400,
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Add Product"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent.shade200,
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
       );
     }
 
@@ -106,28 +103,27 @@ class _Module13State extends State<Module13> {
       ),
 
       body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            childAspectRatio: 0.6,
-          ),
-          itemCount: productController.products.length,
-          itemBuilder: (context,index){
-            return ProductCard(onEdit: (){
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          childAspectRatio: 0.6,
+        ),
+        itemCount: productController.products.length,
+        itemBuilder: (context, index) {
+          return ProductCard(
+            onEdit: () {
               productDialog();
             },
-              onDelete: (){},
-              product: productController.products[index],
-            );
-          }
+            onDelete: () {},
+            product: productController.products[index],
+          );
+        },
       ),
 
       floatingActionButton: FloatingActionButton(
-          onPressed: ()=>productDialog(),
+        onPressed: () => productDialog(),
         child: Icon(Icons.add),
       ),
     );
   }
 }
-
-
